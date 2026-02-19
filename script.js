b7c7069622acc54f254055485915b5212295c041
Prashanth SB <prashanthsb1994@gmail.com>
2026-02-19 19:44:25 +0530

    updated

--- script.js
+++ script.js
@@ -6,11 +6,9 @@ badge.innerText = "Deployment Version: " + version;
 if (version === "v1") {
   badge.style.background = "linear-gradient(90deg,#00c6ff,#0072ff)";
 }
-
 if (version === "v2") {
   badge.style.background = "linear-gradient(90deg,#00ff99,#00cc66)";
 }
-
 if (version === "v3") {
   badge.style.background = "linear-gradient(90deg,#ff4e50,#f9d423)";
 }
