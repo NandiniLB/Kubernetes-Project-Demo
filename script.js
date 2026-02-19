6fa74ddca3c3e71bb0c5c85034a11c03cdd91419
Prashanth SB <prashanthsb1994@gmail.com>
2026-02-19 17:59:37 +0530

    updated the code

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const versionBadge = document.getElementById("versionBadge");
+const version = document.body.getAttribute("data-version");
+const badge = document.getElementById("versionBadge");
 
-const version = "v1";  // Change to v2 for blue-green demo
+badge.innerText = "Deployment Version: " + version;
 
-versionBadge.innerText = "Deployment Version: " + version;
+/* Dynamic Color Switch for Blue-Green Demo */
 
-if(version === "v2"){
-    versionBadge.style.background = "linear-gradient(45deg, #00ff87, #60efff)";
+if (version === "v1") {
+  badge.style.background = "linear-gradient(90deg,#00c6ff,#0072ff)";
+  badge.style.boxShadow = "0 0 15px rgba(0,114,255,0.6)";
+}
+
+if (version === "v2") {
+  badge.style.background = "linear-gradient(90deg,#00ff99,#00cc66)";
+  badge.style.boxShadow = "0 0 15px rgba(0,255,128,0.6)";
+}
+
+if (version === "v3") {
+  badge.style.background = "linear-gradient(90deg,#ff4e50,#f9d423)";
+  badge.style.boxShadow = "0 0 15px rgba(255,100,0,0.6)";
 }
